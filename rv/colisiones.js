2e5fdbe6899279be84b9306f1a11d8a5d7cf06b0
Barrueta <vanebarrueta@hotmail.com>
2016-03-30 10:39:38 -0600

    Create colisiones.js

---
+++ rv/colisiones.js
@@ -0,0 +1,32 @@
+function setup(){
+var cubo=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+var cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+cubo.position.x=0.7;
+cubo2.position.x=-0.7;
+camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+raycaster=new THREE.Raycaster();
+raycaster.setFromCamera(new THREE.Vector2(0,0),camara);
+escena=new THREE.Scene();
+escena.add(cubo);
+escena.add(cubo2);
+escena.add(camara);
+renderer=new THREE.WebGLRenderer();
+renderer.setSize(window.innerHeight*0.95,window.innerHeight*0.95);
+document.doby.appendChild(renderer.domelement);
+step=0.01;
+}
+function loop(){
+var intersects=raycaster.intersectObjects(escena.children);
+if(intersects.legth>0) step=-step;
+cubo.rotation.x +=step;
+cubo.rotation.y +=step;
+cubo2.rotation.x +=step;
+cubo2.rotation.y +=step;
+renderer.render(escena,camara);
+requestAnimationFrame(loop);
+}
+var cubo,cubo2,escena,camara,renderer;
+var raycaster,step;
+setup();
+loop();
