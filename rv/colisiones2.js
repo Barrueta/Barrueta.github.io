34b71cdb4d4fcf2ef425b9dd037c5efd551daecb
Barrueta <vanebarrueta@hotmail.com>
2016-03-30 11:14:02 -0600

    Update colisiones2.js

--- rv/colisiones2.js
+++ rv/colisiones2.js
@@ -22,7 +22,7 @@ step=0.01;
 function loop(){
 obstaculo1=raycaster1.intersectObject(cubo);
 obstaculo2=raycaster2.intersectObject(cubo2);
-if((obstaculo1.length>0 && (obstaculo1[0].distance<=0.5))|| (obstaculo2.length>0 && (obstaculo1[0].distance<=0.5)))
+if((obstaculo1.length>0 && (obstaculo1[0].distance<=0.5))|| (obstaculo2.length>0 && (obstaculo2[0].distance<=0.5)))
 step=-step;
 pelota.position.x +=step
 raycaster1.set(pelota.position,new THREE.Vector3(1,0,0));
