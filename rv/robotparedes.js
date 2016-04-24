ea990eb6d9aa16ffda59b2f64ecda1bee3cfd490
Barrueta <vanebarrueta@hotmail.com>
2016-04-24 14:00:14 -0500

    Update robotparedes.js

--- rv/robotparedes.js
+++ rv/robotparedes.js
@@ -36,7 +36,7 @@ function BB8(x=0, y=0){
  this.cabezabb8 = new BB8Cabeza();
  this.cuerpo.rotation.z=-0.25;
  this.iluminacionr = new THREE.SpotLight(0xffffff,6.4,828,0.07,0,0);
- this.iluminacionr.position.set(Math.sin(this.rotation.z),0,0);//Math.cos(this.rotation.z),0);
+ this.iluminacionr.position.set(Math.cos(this.rotation.z),0,0);//Math.cos(this.rotation.z),0);
  this.add(this.iluminacionr);  
  this.add(this.cuerpo);
  this.add(this.cabezabb8);
@@ -82,8 +82,6 @@ Environment.prototype.setMap=function(map){
 BB8.prototype.sense=function(environment){
  this.sensor.set(this.position, new THREE.Vector3(Math.cos(this.rotation.z),Math.sin(this.rotation.z),0));
  
- 
-
  var obstaculo = this.sensor.intersectObjects(environment.children,true);
  if ((obstaculo.length>0&&(obstaculo[0].distance<=1)))
   this.sensor.colision=true;
