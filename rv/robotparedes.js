3a211f797981f7edb6103c44f5ab75e61fc76672
Barrueta <vanebarrueta@hotmail.com>
2016-04-24 16:01:02 -0500

    Update robotparedes.js

--- rv/robotparedes.js
+++ rv/robotparedes.js
@@ -36,7 +36,7 @@ function BB8(x=0, y=0){
  this.cabezabb8 = new BB8Cabeza();
  this.cuerpo.rotation.z=-0.25;
  this.iluminacionr = new THREE.SpotLight(0xffffff);
- this.iluminacionr.position=Sensor.position;
+ this.iluminacionr.target.position.set(0,200,0);
  this.add(this.iluminacionr);  
  this.add(this.cuerpo);
  this.add(this.cabezabb8);
