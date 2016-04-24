5ec04244335344d4344bb92d086c0d052ca78fd2
Barrueta <vanebarrueta@hotmail.com>
2016-04-24 16:33:07 -0500

    Update robotparedes.js

--- rv/robotparedes.js
+++ rv/robotparedes.js
@@ -26,6 +26,7 @@ function BB8Cabeza(){
  this.add(this.cuello);  
  this.iluminacionr = new THREE.SpotLight(0xffffff);
  this.add(this.iluminacionr);
+ this.iluminacionr.target.position.set(0,0,2);
 
  
 }
@@ -39,7 +40,7 @@ function BB8(x=0, y=0){
  this.cuerpo=new THREE.Mesh(new THREE.SphereGeometry(2,100,100), new THREE.MeshPhongMaterial ({map:textura}));
  this.cabezabb8 = new BB8Cabeza();
  this.cuerpo.rotation.z=-0.25;
- //this.iluminacionr.target.position.set(0,,2);
+ 
   
  this.add(this.cuerpo);
  this.add(this.cabezabb8);
