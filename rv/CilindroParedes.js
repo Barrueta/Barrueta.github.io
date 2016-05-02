ad561885cd4fe0d32395e94d828096826e12f8cb
Barrueta <vanebarrueta@hotmail.com>
2016-05-01 22:10:43 -0500

    Update CilindroParedes.js

--- rv/CilindroParedes.js
+++ rv/CilindroParedes.js
@@ -1,23 +1,30 @@
 function setup(){
-var escena=new THREE.Scene();
+var forma=new THREE.CylinderGeometry(1,1,5,50);
+var material=new THREE.MeshDepthMaterial({color:0x00ff00});
+malla=new THREE.Mesh(forma,material);
+escena=new THREE.Scene();
+escena.add(malla);
+
 var fov=75;
 var aspect=window.innerWidth/window.innerHeight;
 var near=0.1;
 var far=1000;
-var camara=new THREE.PerspectiveCamera(fov,aspect,near,far);
+camara=new THREE.PerspectiveCamera(fov,aspect,near,far);
 camara.position.z=5;
-var renderer=new THREE.WebGLRenderer();
+
+renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var forma=new THREE.CylinderGeometry(1,1,5,50);
-var material=new THREE.MeshDepthMaterial({color:0x00ff00});
-var cilindro=new THREE.Mesh(forma,material);
-escena.add(cilindro);
+
+
 }
-function render()
+function loop()
 {
   requestAnimationFrame(render);
   renderer.render(escena,camara);
 }
-render();
+
+var malla,escena,camara,renderer;
+loop();
+setup();
 
