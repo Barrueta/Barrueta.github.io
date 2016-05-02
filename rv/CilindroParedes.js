9ea76bec9a6420c4b82ff157e4edd64dadbecad0
Barrueta <vanebarrueta@hotmail.com>
2016-05-01 22:42:39 -0500

    Update CilindroParedes.js

--- rv/CilindroParedes.js
+++ rv/CilindroParedes.js
@@ -4,6 +4,9 @@ var textura = new THREE.TextureLoader().load('./jade_dragon.jpg');
 var forma=new THREE.CylinderGeometry(1,1,5,50);
 var material=new THREE.MeshPhongMaterial({map:textura});
 luz=new THREE.PointLight(0xffffff);
+luz.position.x=10;
+luz.position.y=10;
+luz.position.z=10;
 malla=new THREE.Mesh(forma,material);
 malla.material.opacity=0.5;
 escena=new THREE.Scene();
@@ -15,7 +18,7 @@ var aspect=window.innerWidth/window.innerHeight;
 var near=0.1;
 var far=1000;
 camara=new THREE.PerspectiveCamera(fov,aspect,near,far);
-camara.position.z=5;
+camara.position.z=20;
 
 renderer=new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth,window.innerHeight);
